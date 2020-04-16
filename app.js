69bd0dfb99d04f960b3319ec8317d134556fcfc9
inv0w <jonjoninfante@gmail.com>
2020-04-16 03:33:32 -0700

    Adding documents

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create element and render cafe
 function renderCafe(doc){
@@ -15,9 +16,18 @@ function renderCafe(doc){
 
     cafeList.appendChild(li);
 }
-
+// getting data
 db.collection('cafes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     })
+});
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({ 
+        name: form.name.value, 
+        city: form.city.value
+    });
 })
