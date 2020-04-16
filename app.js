681a39b2b54f3c2c582ee8716957e62e1a48e1e6
inv0w <jonjoninfante@gmail.com>
2020-04-16 03:58:40 -0700

    Where searches

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function renderCafe(doc){
     })      
 }
 // getting data
-db.collection('cafes').get().then((snapshot) => {
+db.collection('cafes').where('city', '==', 'San Francisco').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     })
